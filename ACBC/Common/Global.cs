4f5c4f3f6b61e080493ce18bb7eff816a9c21c29
Mitoriko <mitoriko@icloud.com>
2018-12-10 14:28:13 +0800

    服务重启清除缓存信息
    服务重启清除缓存信息

--- ACBC/Common/Global.cs
+++ ACBC/Common/Global.cs
@@ -46,6 +46,8 @@ namespace ACBC.Common
             {
                 Console.WriteLine("Redis Error, Change Local");
             }
+
+            Utils.ClearCache();
         }
 
         public static string SMS_CODE
